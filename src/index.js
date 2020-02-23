409ca5772a759f8bc669e21099cbfadc42bf65ca
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-23 15:40:20 +0600

    socket connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3002;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, ()=> {
+io.on('connection', () => {
+    console.log("New websocket connection !!");
+})
+server.listen(port, ()=> {
     console.log(`Server have started on port ${port}`);
 })
